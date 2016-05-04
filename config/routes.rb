b21265145e3d1da553320206a89a4177dec24b6d
Jack Schuss <jackschuss@gmail.com>
2016-05-04 14:11:31 -0400

    Add venues/tradeshows index routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 namespace :admin do
   resources :sessions, only: [:new, :create, :destroy]
   resources :admin, only: :show
+  resources :venues, only: [:index]
+  resources :tradeshows, only: [:index]
   root to: "sessions#new"
 end
 
