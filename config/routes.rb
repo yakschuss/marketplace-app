281e629449293c2d3fcb98e8aa4d9332fa06e102
Jack Schuss <jackschuss@gmail.com>
2016-05-04 12:59:17 -0400

    Add admin controller views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-
 namespace :admin do
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :admin, only: :show
   root to: "sessions#new"
 end
 
